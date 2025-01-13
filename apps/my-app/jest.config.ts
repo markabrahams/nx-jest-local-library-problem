95a188dd64dd733493249ea9e255045a5eff13fd
Mark Abrahams <mark@abrahams.co.nz>
2025-01-13 13:44:03 +1300

    Fix jest global setup with my-lib dependency failure

--- apps/my-app/jest.config.ts
+++ apps/my-app/jest.config.ts
@@ -4,7 +4,9 @@ export default {
   coverageDirectory: '../../coverage/apps/my-app',
   globalSetup: '<rootDir>/src/tests/jest.setup.ts',
   testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@nx-jest-local-library-problem/my-lib$': '<rootDir>/../../libs/my-lib/src/index.ts',
-  },
+  // moduleNameMapper should be used by the globalSetup script, but it isn't at the time of writing
+  // https://github.com/jestjs/jest/issues/11644
+  // moduleNameMapper: {
+  //   '^@nx-jest-local-library-problem/my-lib$': '<rootDir>/../../libs/my-lib/src/index.ts',
+  // },
 };
--- apps/my-app/src/tests/jest.setup.ts
+++ apps/my-app/src/tests/jest.setup.ts
@@ -1,3 +1,8 @@
+// Fixes Error: Jest: Got error running globalSetup - /home/mark/node/nx-jest-local-library-problem/apps/my-app/src/tests/jest.setup.ts, reason: Cannot find module '@nx-jest-local-library-problem/my-lib'
+// Issue reported: https://github.com/jestjs/jest/issues/11644
+// Successful fix: https://github.com/jestjs/jest/issues/11644#issuecomment-1171646729
+import 'tsconfig-paths/register';
+
 import { hello } from '@nx-jest-local-library-problem/my-lib';
 
 module.exports = async () => {
